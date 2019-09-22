fef34eb62f4f235238935e45d8570d99c15defec
Kevin <mrhallonline@gmail.com>
2019-09-22 18:38:50 -0500

    completed course by adding smooth scrolling, and animations. Done!

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,65 @@
-$(document).ready(function()  {
+/*****For Sticky Navigation */
 
-    $('.js--section-features').waypoint(function(direction) {
-        if(direction=="down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '60px;'
-      });
+$(document).ready(function () {
+
+  $('.js--section-features').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+
+  /*****Scroll on Buttons****/
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000);
+  });
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+  });
+
+/****************Smooth Scrolling from top links */
+/***used CCS */
+
+/***animation on scroll */
+$('.js--wp-1').waypoint(function(direction) { 
+    $('.js--wp-1').addClass('animated fadeIn');
+},  {
+  offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction) { 
+  $('.js--wp-2').addClass('animated fadeInUp');
+},  {
+offset: '50%'
+});
+
+$('.js--wp-3').waypoint(function(direction) { 
+  $('.js--wp-3').addClass('animated fadeIn');
+},  {
+offset: '50%'
+});
+
+$('.js--wp-4').waypoint(function(direction) { 
+  $('.js--wp-4').addClass('animated pulse');
+},  {
+offset: '50%'
+});
+
+/***mobile-navigation****/
+$('.js--nav-icon').click(function() {
+var nav = $('.js--main-nav')
+var icon = $('js--nav-icon ion-icon')
+nav.slideToggle(200);
 
 
-/*
-var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})
-*/
+});
+
 
 });
